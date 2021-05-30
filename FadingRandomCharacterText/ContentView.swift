08d260f3a17d304ec5b6e3e514ae6b035a387cb9
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-05-29 22:49:13 -0700

    First Commit

--- FadingRandomCharacterText/ContentView.swift
+++ FadingRandomCharacterText/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var opacityManager = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(spacing: 0){
+        Text("W")
+            .font(.title)
+           
+            .opacity(opacityManager ? 1 : 0.2)
+            .animation(
+                Animation.easeInOut(duration: 1.5)
+                    .delay(0.9)
+                    .repeatForever()
+            )
+            Text("O")
+                .font(.title)
+              
+                .opacity(opacityManager ? 1 : 0.4)
+                .animation(
+                    Animation.easeInOut(duration: 1)
+                        .delay(1.5)
+                        .repeatForever()
+                )
+            Text("R")
+                .font(.title)
+                
+                .opacity(opacityManager ? 1 : 0.3)
+                .animation(
+                    Animation.easeInOut(duration: 2)
+                        .delay(1.25)
+                        .repeatForever()
+                )
+            Text("L")
+                .font(.title)
+               
+                .opacity(opacityManager ? 1 : 0.1)
+                .animation(
+                    Animation.easeInOut(duration: 1.8)
+                        .delay(1)
+                        .repeatForever()
+                )
+            Text("D")
+                .font(.title)
+                
+                .opacity(opacityManager ? 1 : 0.2)
+                .animation(
+                    Animation.easeInOut(duration: 0.8)
+                        .delay(0.8)
+                        .repeatForever()
+                )
+            
+            }.onAppear{
+                withAnimation {
+                    opacityManager.toggle()
+                }
+            }.padding()
+        .background(Color.yellow)
     }
 }
 
