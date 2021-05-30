c1ff8460daa909beec5864694b4db54d26717021
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-05-29 22:26:39 -0700

    Initial Commit

---
+++ FadingRandomCharacterText/FadingRandomCharacterTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  FadingRandomCharacterTextApp.swift
+//  FadingRandomCharacterText
+//
+//  Created by Vadiraj Hippargi on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct FadingRandomCharacterTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
